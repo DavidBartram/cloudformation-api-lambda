d83769fb6306d881df8b01f359f43b08892889d6
dbartram <dbartram@dxc.com>
2021-07-27 17:15:21 +0100

    simplify lambda code

--- lambda_code/lambda_function.py
+++ lambda_code/lambda_function.py
@@ -1,12 +1,5 @@
-import json
-
 def lambda_handler(event, context):
 
-    try:
-        message = f"Thank you for submitting data. You submitted the following. Location was {event['location']}, number of red widgets sold was {event['red_sold']}, number of blue widgets sold was {event['blue_sold']}, number of green widgets sold was {event['green sold']}"
-    
-    except:
-        returnData = {"Notes":"Failed to receive data"}
-                
-    
-    return returnData
+    message = f"Thank you for submitting data. You submitted the following. Date {event['date']} at location {event['location']}, number of red widgets sold was {event['red_sold']}, number of blue widgets sold was {event['blue_sold']}, number of green widgets sold was {event['green_sold']}."
+
+    return {"Notes": message}
