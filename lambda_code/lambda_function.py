ffacf7cc6e0cdd119f712c52f55ff8d4c083d112
DavidBartram <60873806+DavidBartram@users.noreply.github.com>
2021-07-27 16:24:23 +0100

    Create lambda_function.py

---
+++ lambda_code/lambda_function.py
@@ -0,0 +1,13 @@
+import json
+
+def lambda_handler(event, context)
+
+    try:
+        message = f"Thank you for submitting data. You submitted the following. Location was {event['location']}, number of red widgets sold was {event['red_sold']}, number of blue widgets sold was {event['blue_sold']}, number of green widgets sold was {event['green sold']}"
+      
+    
+    except:
+        returnData = {"Notes":"Failed to receive data"}
+                
+    
+    return returnData
